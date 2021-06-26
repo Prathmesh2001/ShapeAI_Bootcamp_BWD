caf639a8fa1912039b70f04294a2efc018e55b7b
Prathmesh2001 <68183030+Prathmesh2001@users.noreply.github.com>
2021-06-26 19:05:51 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and ReactJs</h1>
+      <p>
+        A basic web dev bootcamp. Have an overview of frontend development.Hands
+        on Project.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
